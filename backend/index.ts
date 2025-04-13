dd894d23594181a059a8f78bb9f49fef966c3d7d
armiino <armino@tuta.io>
2025-04-13 17:07:26 +0200

    add authRoute to index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import calculateRoute from './route/calculateRoute';
+import authRoute from './route/authRoute';
+import dotenv from 'dotenv';
 
+dotenv.config(); 
 
 const app = express();
 const PORT = 8000;
@@ -8,6 +11,7 @@ const PORT = 8000;
 app.use(express.json());
 
 app.use('/api', calculateRoute);
+app.use('/auth', authRoute);
 
 //kurzer test obs läuft
 app.get('/', (req: Request, res: Response) => {
