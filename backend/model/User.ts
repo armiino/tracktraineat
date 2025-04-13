317c8fda66125406c5191944df8cbd1b667569db
armiino <armino@tuta.io>
2025-04-13 17:12:46 +0200

    add User Model

---
+++ backend/model/User.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs';
+
+export class User {
+  constructor(
+    public id: string,
+    public email: string,
+    private passwordHash: string,
+    public role: 'user' | 'admin' = 'user'
+  ) {}
+
+  checkPassword(plainPassword: string): boolean {
+    return bcrypt.compareSync(plainPassword, this.passwordHash);
+  }
+
+  // infos fürs frontend 
+  getPublicProfile() {
+    return {
+      id: this.id,
+      email: this.email,
+      role: this.role
+    };
+  }
+
+  // user erstellen
+  // password wird gehashed
+  // random uid
+  // role hier egal und wird nicht mitgegeben -> registerUserDto besteht nur aus email und password -> egal was man im body mitgibt es wird immer auf user gesetzt
+  // plan ist erst mal über die DB die rechte zu verwalten - später vllt mit funktionen
+  static async create(email: string, plainPassword: string): Promise<User> {
+    const hashed = await bcrypt.hash(plainPassword, 10); // 10 runden Salting
+    const id = crypto.randomUUID(); // später db id ?? - aktuell random generiert
+    return new User(id, email, hashed, 'user');
+  }
+}
