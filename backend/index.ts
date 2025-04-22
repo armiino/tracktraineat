be51730ddd3b8915cdde27199a37da3ccc036444
armiino <armino@tuta.io>
2025-04-22 13:41:29 +0200

    add cookieParser | move dotenv.config to the top

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config(); //muss vor allen imports kommen sonst wird der wert "zu spät" erkannt (fix warum dotenv value nicht genutzt wurde)
 import express, { Request, Response } from 'express';
 import calculateRoute from './route/calculateRoute';
 import authRoute from './route/authRoute';
-import dotenv from 'dotenv';
-
-dotenv.config(); 
+import testMiddlewareRoute from './route/testMiddlewareRoute'; // TODO: wieder entfernen - dient nur zu testzwecken der Middleware
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 
+app.use(cookieParser()); 
+
 app.use('/api', calculateRoute);
 app.use('/auth', authRoute);
+app.use('/test', testMiddlewareRoute); // TODO: wieder entfernen
 
 //kurzer test obs läuft
 app.get('/', (req: Request, res: Response) => {
