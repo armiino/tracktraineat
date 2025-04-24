2e4a543f9658de6695eaf31b734afd3778a978a3
armiino <armino@tuta.io>
2025-04-24 11:44:03 +0200

    add userProfileService

---
+++ backend/service/userProfileService.ts
@@ -0,0 +1,53 @@
+import { UserProfile } from '../model/UserProfile';
+import { UserProfileDto } from '../dto/UserProfileDto';
+
+const profiles: UserProfile[] = []; //TODO: array funkton weg -> DB 
+
+export const userProfileService = {
+  createProfile(userId: string, dto: UserProfileDto): UserProfile {
+    const profile = new UserProfile(
+      userId,
+      dto.weight,
+      dto.height,
+      dto.age,
+      dto.gender,
+      dto.activity,
+      dto.goal,
+      dto.burned
+    );
+
+    if (profiles.find(p => p.userId === userId)) {
+        throw new Error('Profile already exists');
+      }
+    profiles.push(profile);
+    return profile;
+  },
+
+  getProfile(userId: string): UserProfile | undefined { //undefinded falls keins gefundn wird (typescript stresst hier)
+    return profiles.find(function(p) { return p.userId === userId});
+  },
+
+  updateProfile(userId: string, dto: UserProfileDto): UserProfile {
+    const profileIndex = profiles.findIndex(p => p.userId === userId); 
+  
+    if (profileIndex === -1) {
+      throw new Error('Profile not found');
+    }
+  
+    // Update-Profil
+    const updated = new UserProfile(
+      userId,
+      dto.weight,
+      dto.height,
+      dto.age,
+      dto.gender,
+      dto.activity,
+      dto.goal,
+      dto.burned
+    );
+  
+    profiles[profileIndex] = updated;
+    return updated;
+  }
+  
+};
