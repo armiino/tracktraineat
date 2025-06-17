f98d09af88a847588ef38c4262b9cfd78ff823b7
armiino <armino@tuta.io>
2025-06-17 16:30:20 +0200

    added new ports for User and UserProfile for db

---
+++ backend/port/UserProfileRepository.ts
@@ -0,0 +1,7 @@
+import { UserProfile } from '../model/UserProfile';
+
+export interface UserProfileRepository {
+  save(profile: UserProfile): Promise<void>;
+  findByUserId(userId: string): Promise<UserProfile | null>;
+  update(profile: UserProfile): Promise<void>;
+}
