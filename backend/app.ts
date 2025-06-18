666b048c83d11ef87d17bc3e4601eadfbcced71a
armiino <armino@tuta.io>
2025-06-18 17:27:04 +0200

    seperate app.listen from index.ts

---
+++ backend/app.ts
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+dotenv.config(); //muss vor allen imports kommen sonst wird der wert "zu spät" erkannt (fix warum dotenv value nicht genutzt wurde)
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import calculateRoute from './route/calculateRoute';
+import authRoute from './route/authRoute';
+import testMiddlewareRoute from './route/testMiddlewareRoute'; // TODO: wieder entfernen - dient nur zu testzwecken der Middleware
+//import userProfileRoute from './route/userProfileRoute';
+import calculateProfileRoute from './route/calculateProfileRoute';
+import { createRecipeRoute } from './route/recipeRoute';
+import { createUserProfileRoute } from './route/userProfileRoute';
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api', calculateRoute); //offener endpunkt und kalulkuliert den Gesamtumsatz
+//app.use('/api', userProfileRoute); //profil anlegen oder updaten
+app.use('/auth', authRoute); //register und login TODO: evtl auch auf factory umziehen
+app.use('/test', testMiddlewareRoute); // TODO: wieder entfernen
+app.use('/api', calculateProfileRoute);
+app.use('/api', createRecipeRoute()); 
+app.use('/api', createUserProfileRoute());
+
+//kurzer test obs läuft
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server läuft');
+});
+
+export { app };
--- backend/index.ts
+++ backend/index.ts
@@ -1,36 +1,10 @@
 import dotenv from 'dotenv';
-dotenv.config(); //muss vor allen imports kommen sonst wird der wert "zu spät" erkannt (fix warum dotenv value nicht genutzt wurde)
-import express, { Request, Response } from 'express';
-import cookieParser from 'cookie-parser';
-import calculateRoute from './route/calculateRoute';
-import authRoute from './route/authRoute';
-import testMiddlewareRoute from './route/testMiddlewareRoute'; // TODO: wieder entfernen - dient nur zu testzwecken der Middleware
-//import userProfileRoute from './route/userProfileRoute';
-import calculateProfileRoute from './route/calculateProfileRoute';
-import { createRecipeRoute } from './route/recipeRoute';
-import { createUserProfileRoute } from './route/userProfileRoute';
+dotenv.config();
 
-const app = express();
-const PORT = 8000;
+import { app } from './app';
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/api', calculateRoute); //offener endpunkt und kalulkuliert den Gesamtumsatz
-//app.use('/api', userProfileRoute); //profil anlegen oder updaten
-app.use('/auth', authRoute); //register und login TODO: evtl auch auf factory umziehen
-app.use('/test', testMiddlewareRoute); // TODO: wieder entfernen
-app.use('/api', calculateProfileRoute);
-app.use('/api', createRecipeRoute()); 
-app.use('/api', createUserProfileRoute());
-
-//kurzer test obs läuft
-app.get('/', (req: Request, res: Response) => {
-  res.send('Server läuft');
-});
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
 });
-
-export default app;
