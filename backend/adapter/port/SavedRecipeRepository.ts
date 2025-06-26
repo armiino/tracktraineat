a9fc2097b19ca94b67a892866a5e4885fe1d68e1
armiino <armino@tuta.io>
2025-06-26 14:18:54 +0200

    add new port for saved recipe adapter

---
+++ backend/adapter/port/SavedRecipeRepository.ts
@@ -0,0 +1,8 @@
+import { SavedRecipeModel } from '../../model/SavedRecipe';
+import { SavedRecipeCreateInput } from '../../model/SavedRecipe';
+
+export interface SavedRecipeRepository {
+  saveRecipe(data: SavedRecipeCreateInput): Promise<SavedRecipeModel>;
+  getSavedRecipes(userId: string): Promise<SavedRecipeModel[]>;
+  deleteSavedRecipe(userId: string, spoonId: number): Promise<void>;
+}
